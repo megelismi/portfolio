a0c6f986c72ea9b6a317232ff3ff7bb21457fc96
Megan Smith <meg.eli.smi@gmail.com>
2017-01-30 14:41:26 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("#bars li .bar").each( function( key, bar ) {
+  	// console.log('key', key)
+  	console.log('bar', bar)
+  	console.log('this', this)
+    var percentage = $(this).data('percentage');
+
+    $(this).animate({
+      'height' : percentage + '%'
+    }, 1000);
+  });
+});
