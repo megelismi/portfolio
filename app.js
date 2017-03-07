1be00431fad8ccc9ea2bb0eb7795c000f3cfe4b5
Megan Smith <meg.eli.smi@gmail.com>
2017-03-07 15:26:15 -0800

    add pictures to work section, details div for edster and localize working

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-// $(function() {
-//   $("#bars li .bar").each( function( key, bar ) {
-//   	// console.log('key', key)
-//   	console.log('bar', bar)
-//   	console.log('this', this)
-//     var percentage = $(this).data('percentage');
 
-//     $(this).animate({
-//       'height' : percentage + '%'
-//     }, 1000);
-//   });
-// });
+$(function() {
+
+	$('#localize-more').click(function() {
+	  $('#localize-details-container').removeClass('hidden'); 
+	  window.location.hash = '#localize-details-container';
+	});
+
+	$('#close-localize').click(function() {
+  $('#localize-details-container').addClass('hidden'); 
+});
+
+
+	$('#edster-more').click(function() {
+		console.log('this', this)
+	  $('#edster-details-container').removeClass('hidden'); 
+	  window.location.hash = '#edster-details-container';
+	});
+
+	$('#close-edster').click(function() {
+			console.log('something happened');
+	  $('#edster-details-container').addClass('hidden'); 
+	});
+
+	$('#kapitol-more').click(function() {
+		console.log('this', this)
+	  $('#kapitol-details-container').removeClass('hidden'); 
+	  window.location.hash = '#kapitol-details-container';
+	});
+
+	$('#close-kapitol').click(function() {
+			console.log('something happened');
+	  $('#kapitol-details-container').addClass('hidden'); 
+	});
+
+	$('#yaarn-more').click(function() {
+		console.log('this', this)
+	  $('#yaarn-details-container').removeClass('hidden'); 
+	  window.location.hash = '#yaarn-details-container';
+	});
+
+	$('#close-yaarn').click(function() {
+		console.log('something happened');
+  $('#yaarn-details-container').addClass('hidden'); 
+});
+
+});
