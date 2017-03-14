aa1c5d2114d328e7a216c2ee2babced6ff40edd5
Megan Smith <meg.eli.smi@gmail.com>
2017-03-13 20:49:58 -0700

    work scrolling works

--- app.js
+++ app.js
@@ -77,26 +77,42 @@ $(function() {
  
 });
 
+var hasBoundScroll = false; 
+
+ var onScroll = function() {
+ 	
+	 	yaarnDistance = $('#yaarn-container').offset().top,
+		localizeDistance = $('#localize-container').offset().top;
+
+ 		if ( $(window).scrollTop() >= localizeDistance ) {
+	   	$("#work-label-container").addClass("fixed").removeClass("absolute");
+	  } else {
+	    $("#work-label-container").removeClass("fixed");
+	  }
+
+	  if ($(window).scrollTop() >= yaarnDistance) {
+	    $("#work-label-container").addClass("absolute").removeClass("fixed");
+	  }
+	}
+
 function updateContainer() {
-	console.log('update container called');
 	var workDistance = $('#work-label-container').offset().top,
 	yaarnDistance = $('#yaarn-container').offset().top,
 	localizeDistance = $('#localize-container').offset().top,
 	$window = $(window);
   var $width = $(window).width();
-  console.log('width', $width)
-  if ($width >= 822) {
-    $window.scroll(function() {
-			if ( $window.scrollTop() >= localizeDistance ) {
-		   	$("#work-label-container").addClass("fixed").removeClass("absolute");
-		  } else {
-		    $("#work-label-container").removeClass("fixed");
-		  }
-		   if ($window.scrollTop() >= yaarnDistance) {
-		    	$("#work-label-container").addClass("absolute").removeClass("fixed");
-		   }
-		}); 
-  } else {
-  	$("#work-label-container").removeClass("absolute").removeClass("fixed");
+
+
+	console.log($("#work-label-container"));
+  if ($width >= 768 && !hasBoundScroll) {
+  	console.log('window width is greater than cutoff:', $width);
+		$window.on('scroll', onScroll); 
+		hasBoundScroll = true;
+  } else if (hasBoundScroll && $width < 768) {
+  		console.log('window width is less than cutoff:', $width);
+  		$window.off('scroll', onScroll);
+  		$("#work-label-container").removeClass("absolute fixed");
+  		hasBoundScroll = false;
   }
+
 }
