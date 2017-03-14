3ff23d139845319efd92faebd550496c8f213409
Megan Smith <meg.eli.smi@gmail.com>
2017-03-13 20:59:10 -0700

    floating nav expands to full width of screen

--- app.js
+++ app.js
@@ -68,48 +68,44 @@ $(function() {
       }
   });
 
- updateContainer();
-
- $(window).resize(function() {
- 		console.log('window resize called')
- 		updateContainer();
- });
- 
+	updateContainer();
+	
+	$(window).resize(function() {
+	 	updateContainer();
+	});
 });
 
 var hasBoundScroll = false; 
 
- var onScroll = function() {
- 	
-	 	yaarnDistance = $('#yaarn-container').offset().top,
-		localizeDistance = $('#localize-container').offset().top;
+var onScroll = function() {
 
- 		if ( $(window).scrollTop() >= localizeDistance ) {
-	   	$("#work-label-container").addClass("fixed").removeClass("absolute");
-	  } else {
-	    $("#work-label-container").removeClass("fixed");
-	  }
+	yaarnDistance = $('#yaarn-container').offset().top,
+	localizeDistance = $('#localize-container').offset().top;
 
-	  if ($(window).scrollTop() >= yaarnDistance) {
-	    $("#work-label-container").addClass("absolute").removeClass("fixed");
-	  }
-	}
+	if ( $(window).scrollTop() >= localizeDistance ) {
+   	$("#work-label-container").addClass("fixed").removeClass("absolute");
+  } else {
+    $("#work-label-container").removeClass("fixed");
+  }
+
+  if ($(window).scrollTop() >= yaarnDistance) {
+    $("#work-label-container").addClass("absolute").removeClass("fixed");
+  }
+
+}
 
 function updateContainer() {
+
 	var workDistance = $('#work-label-container').offset().top,
 	yaarnDistance = $('#yaarn-container').offset().top,
 	localizeDistance = $('#localize-container').offset().top,
 	$window = $(window);
   var $width = $(window).width();
 
-
-	console.log($("#work-label-container"));
   if ($width >= 768 && !hasBoundScroll) {
-  	console.log('window width is greater than cutoff:', $width);
 		$window.on('scroll', onScroll); 
 		hasBoundScroll = true;
   } else if (hasBoundScroll && $width < 768) {
-  		console.log('window width is less than cutoff:', $width);
   		$window.off('scroll', onScroll);
   		$("#work-label-container").removeClass("absolute fixed");
   		hasBoundScroll = false;
